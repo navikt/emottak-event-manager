2060a7ea3c372bb5ef1e2a7f4934d491ceb0c31a
Oleksandr Chmyr <oleksandr.chmyr@nav.no>
2025-02-28 11:44:07 +0100

    Fikset Application.kt

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,9 @@ dependencies {
     implementation("io.ktor:ktor-server-netty")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("io.ktor:ktor-server-config-yaml")
+    implementation("io.ktor:ktor-server-content-negotiation:2.3.6")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.6")
+    api("dev.reformator.stacktracedecoroutinator:stacktrace-decoroutinator-jvm:2.3.8")
     testImplementation("io.ktor:ktor-server-test-host")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,11 +1,33 @@
-package no.nav
+package no.nav.emottak.eventmanager
 
 import io.ktor.server.application.*
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+import dev.reformator.stacktracedecoroutinator.runtime.DecoroutinatorRuntime
+import io.ktor.serialization.kotlinx.json.json
+import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.server.response.respondText
+import io.ktor.server.routing.get
+import io.ktor.server.routing.routing
 
 fun main(args: Array<String>) {
-    io.ktor.server.netty.EngineMain.main(args)
+    //if (getEnvVar("NAIS_CLUSTER_NAME", "local") != "prod-fss") {
+        DecoroutinatorRuntime.load()
+    //}
+    embeddedServer(
+        factory = Netty,
+        port = 8080,
+        module = eventManagerModule()
+    ).start(wait = true)
 }
 
-fun Application.module() {
-    configureRouting()
+fun eventManagerModule(): Application.() -> Unit {
+    return {
+        install(ContentNegotiation) { json() }
+        routing {
+            get("/") {
+                call.respondText("Hello, World!")
+            }
+        }
+    }
 }
