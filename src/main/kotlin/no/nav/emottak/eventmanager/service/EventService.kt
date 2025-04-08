d9e8e2c50943d675609b861c72389f34d9bee333
OleksandrChmyrNAV <oleksandr.chmyr@nav.no>
2025-04-08 14:25:59 +0200

    #178 Koble /fetchevents endepunkt til database (#11)
    * Utført henting av hendelser i et tidsrom
    
    * Lagt til test for EventRepository.findEventByTimeInterval()
    
    * Lagt til integrasjonstest for /fetchevents endepunkt
    
    * Refaktorert /fetchevents endepunkt

--- src/main/kotlin/no/nav/emottak/eventmanager/service/EventService.kt
+++ src/main/kotlin/no/nav/emottak/eventmanager/service/EventService.kt
@@ -2,8 +2,10 @@ package no.nav.emottak.eventmanager.service
 
 import kotlinx.serialization.json.Json
 import no.nav.emottak.eventmanager.log
+import no.nav.emottak.eventmanager.model.EventInfo
 import no.nav.emottak.eventmanager.persistence.repository.EventsRepository
 import no.nav.emottak.utils.kafka.model.Event
+import java.time.Instant
 
 class EventService(private val eventsRepository: EventsRepository) {
     suspend fun process(value: ByteArray) {
@@ -15,6 +17,17 @@ class EventService(private val eventsRepository: EventsRepository) {
             log.info("Event processed successfully: $event")
         } catch (e: Exception) {
             log.error("Exception while processing event:${String(value)}", e)
+        }
+    }
+
+    suspend fun fetchEvents(from: Instant, to: Instant): List<EventInfo> {
+        return eventsRepository.findEventByTimeInterval(from, to).map {
+            EventInfo(
+                hendelsedato = it.createdAt.toString(),
+                hendelsedeskr = it.eventType.toString(),
+                tillegsinfo = it.eventData.toString(),
+                mottakid = it.requestId.toString()
+            )
         }
     }
 }
