7b9d64f04d52e9c752b161ce987a80059e3576dd
Oleksandr Chmyr <oleksandr.chmyr@nav.no>
2025-07-15 15:34:03 +0200

    Lagt til tester for /fetchMessageLoggInfo endepunkt

--- src/main/kotlin/no/nav/emottak/eventmanager/model/MessageLoggInfo.kt
+++ src/main/kotlin/no/nav/emottak/eventmanager/model/MessageLoggInfo.kt
@@ -1,5 +1,8 @@
 package no.nav.emottak.eventmanager.model
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class MessageLoggInfo(
     val hendelsesdato: String,
     val hendelsesbeskrivelse: String,
