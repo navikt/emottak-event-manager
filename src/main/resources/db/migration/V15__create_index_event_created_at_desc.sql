3766eac08af8eb187712a63d3352f87ac0a5fc90
terjenilssen <terje.nilssen@nav.no>
2026-02-06 12:13:54 +0100

    Korrigert tabellnavn.

--- src/main/resources/db/migration/V15__create_index_event_created_at_desc.sql
+++ src/main/resources/db/migration/V15__create_index_event_created_at_desc.sql
@@ -1,1 +1,1 @@
-CREATE INDEX idx_event_msg_created_at_desc ON event (request_id, created_at DESC, event_type_id);
+CREATE INDEX "idx_event_msg_created_at_desc" ON "events" (request_id, created_at DESC, event_type_id);
