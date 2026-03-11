0271544e2c4e62039699c543b21eede0da196984
terjenilssen <terje.nilssen@nav.no>
2026-03-11 12:25:37 +0100

    Oppdatert ktor-test til versjon 3.3.1. Endret string-parameter fra å være angitt med anførselstegn, til å være angitt med enkeltsnutt. Har for sikkerhetsskyld også endret SQL-spørringer til å angi tabell-alias for kolonne. Vet ikke om VARCHAR(256) vs TEXT, eller int vs INTEGER har noe å si.

--- src/main/resources/db/migration/V15__define_delete_service_events_procedure.sql
+++ src/main/resources/db/migration/V15__define_delete_service_events_procedure.sql
@@ -1,20 +1,20 @@
 CREATE OR REPLACE PROCEDURE delete_service_events(
-    p_service VARCHAR(256),
-    p_batch_size INTEGER DEFAULT 10000
+    p_service text,
+    p_batch_size int DEFAULT 10000
 )
 LANGUAGE plpgsql
 AS $$
 DECLARE
-    v_deleted_count INTEGER;
+    v_deleted_count int;
     v_total_deleted_events BIGINT := 0;
     v_total_deleted_conversations BIGINT := 0;
     v_total_deleted_message_details BIGINT := 0;
     v_request_ids UUID[];
-    v_conversation_ids VARCHAR(256)[];
+    v_conversation_ids text[];
     v_conversation_status_table_exists BOOLEAN;
 BEGIN
     -- Hopp over sletting hvis ebms_message_details ikke lenger inneholder service som skal slettes:
-    IF NOT EXISTS (SELECT 1 FROM ebms_message_details WHERE service = p_service LIMIT 1) THEN
+    IF NOT EXISTS (SELECT 1 FROM ebms_message_details m WHERE m.service = p_service LIMIT 1) THEN
         RAISE NOTICE 'No rows found for service %, skipping deletion', p_service;
         RETURN;
     END IF;
@@ -24,12 +24,12 @@ BEGIN
 
     LOOP
         -- Hent relevante request_id'er og conversation_id'er som skal slettes:
-        SELECT ARRAY_AGG(request_id), ARRAY_AGG(conversation_id)
+        SELECT ARRAY_AGG(m.request_id), ARRAY_AGG(m.conversation_id)
         INTO v_request_ids, v_conversation_ids
         FROM (
-             SELECT request_id, conversation_id
-             FROM ebms_message_details
-             WHERE service = p_service
+             SELECT m.request_id, m.conversation_id
+             FROM ebms_message_details m
+             WHERE m.service = p_service
              LIMIT p_batch_size
          ) sub;
 
@@ -39,24 +39,24 @@ BEGIN
         END IF;
 
         -- Slett hendelser:
-        DELETE FROM events
-        WHERE request_id = ANY(v_request_ids);
+        DELETE FROM events e
+        WHERE e.request_id = ANY(v_request_ids);
 
         GET DIAGNOSTICS v_deleted_count = ROW_COUNT;
         v_total_deleted_events := v_total_deleted_events + v_deleted_count;
 
         -- Slett conversations:
         IF v_conversation_status_table_exists THEN
-            DELETE FROM conversation_status
-            WHERE conversation_id = ANY(v_conversation_ids);
+            DELETE FROM conversation_status c
+            WHERE c.conversation_id = ANY(v_conversation_ids);
 
             GET DIAGNOSTICS v_deleted_count = ROW_COUNT;
             v_total_deleted_conversations := v_total_deleted_conversations + v_deleted_count;
         END IF;
 
         -- Og til slutt slett meldingsdetaljer:
-        DELETE FROM ebms_message_details
-        WHERE request_id = ANY(v_request_ids);
+        DELETE FROM ebms_message_details m
+        WHERE m.request_id = ANY(v_request_ids);
 
         GET DIAGNOSTICS v_deleted_count = ROW_COUNT;
         v_total_deleted_message_details := v_total_deleted_message_details + v_deleted_count;
