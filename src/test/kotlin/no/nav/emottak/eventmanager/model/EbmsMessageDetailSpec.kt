5c19eab145c67f68489f1962ddf3d322d8f36118
terjenilssen <terje.nilssen@nav.no>
2026-06-05 13:44:48 +0200

    Midlertidig fjernet nesten alle default-verdier for navPartyIds for å teste at miljøvariabler blir lest korrekt i DEV.

--- src/test/kotlin/no/nav/emottak/eventmanager/model/EbmsMessageDetailSpec.kt
+++ src/test/kotlin/no/nav/emottak/eventmanager/model/EbmsMessageDetailSpec.kt
@@ -11,34 +11,34 @@ import no.nav.emottak.utils.common.parseOrGenerateUuid
 class EbmsMessageDetailSpec : DescribeSpec({
 
     describe("Tests of direction") {
-
+/*
         it("Should return Direction OUT when fromPartyId is 'HER:79768'") {
             val messageDetail = buildTestEbmsMessageDetail().copy(fromPartyId = "HER:79768")
             messageDetail.getDirection() shouldBe "OUT"
         }
-
-        it("Should return Direction OUT when fromPartyId is 'ENH:889640782'") {
-            val messageDetail = buildTestEbmsMessageDetail().copy(fromPartyId = "ENH:889640782")
+*/
+        it("Should return Direction OUT when fromPartyId is 'ENH:990983291'") {
+            val messageDetail = buildTestEbmsMessageDetail().copy(fromPartyId = "ENH:990983291")
             messageDetail.getDirection() shouldBe "OUT"
         }
 
-        it("Should return Direction OUT when fromPartyId is 'orgnummer:889640782'") {
-            val messageDetail = buildTestEbmsMessageDetail().copy(fromPartyId = "orgnummer:889640782")
+        it("Should return Direction OUT when fromPartyId is 'orgnummer:990983291'") {
+            val messageDetail = buildTestEbmsMessageDetail().copy(fromPartyId = "orgnummer:990983291")
             messageDetail.getDirection() shouldBe "OUT"
         }
-
+/*
         it("Should return Direction OUT when fromPartyId is 'commonname:ARBEIDS OG VELFERDSETATEN'") {
             val messageDetail = buildTestEbmsMessageDetail().copy(fromPartyId = "commonname:ARBEIDS OG VELFERDSETATEN")
             messageDetail.getDirection() shouldBe "OUT"
         }
-
-        it("Should return Direction IN when fromPartyId is not representing Nav (none of the values in config().navPartyIds)") {
+*/
+        it("Should return Direction IN when fromPartyId is not representing Nav (none of the values in config().navPartyIdsList)") {
             val messageDetail = buildTestEbmsMessageDetail()
             messageDetail.getDirection() shouldBe "IN"
         }
 
-        it("Should still return Direction IN when fromPartyId is similar but not identical to a value in config().navPartyIds") {
-            val messageDetail = buildTestEbmsMessageDetail().copy(fromPartyId = "ARBEIDS OG VELFERDSETATEN")
+        it("Should still return Direction IN when fromPartyId is similar but not identical to a value in config().navPartyIdsList") {
+            val messageDetail = buildTestEbmsMessageDetail().copy(fromPartyId = "ENH:990983290")
             messageDetail.getDirection() shouldBe "IN"
         }
     }
@@ -46,7 +46,7 @@ class EbmsMessageDetailSpec : DescribeSpec({
     describe("Tests of readable id") {
 
         it("Should return a readable id starting on OUT. when fromPartyId is Nav") {
-            val messageDetail = buildTestEbmsMessageDetail().copy(fromPartyId = "HER:79768")
+            val messageDetail = buildTestEbmsMessageDetail().copy(fromPartyId = "orgnummer:990983291")
             messageDetail.generateReadableId() shouldStartWith "OUT."
         }
 
@@ -56,7 +56,7 @@ class EbmsMessageDetailSpec : DescribeSpec({
         }
 
         it("Should return a readable id containing 'NAVM' when fromPartyId is Nav") {
-            val messageDetail = buildTestEbmsMessageDetail().copy(fromPartyId = "ENH:889640782")
+            val messageDetail = buildTestEbmsMessageDetail().copy(fromPartyId = "ENH:990983291")
             messageDetail.generateReadableId() shouldContain ".NAVM."
         }
 
@@ -70,10 +70,10 @@ class EbmsMessageDetailSpec : DescribeSpec({
             messageDetail.generateReadableId() shouldContain ".oslo."
         }
 
-        it("Should return a readable id containing 'NAVM' when senderName is 'Something but fromPartyId is Nav") {
+        it("Should return a readable id containing 'NAVM' when senderName is 'Something' but fromPartyId is Nav") {
             val messageDetail = buildTestEbmsMessageDetail().copy(
                 senderName = "Something",
-                fromPartyId = "commonname:ARBEIDS OG VELFERDSETATEN"
+                fromPartyId = "ENH:990983291"
             )
             messageDetail.generateReadableId() shouldContain ".NAVM."
         }
