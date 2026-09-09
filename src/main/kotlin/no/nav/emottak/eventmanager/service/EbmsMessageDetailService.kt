365d86c1dda62d51fdc4c6c5c799cc143b59ac12
terjenilssen <terje.nilssen@nav.no>
2026-09-09 14:25:43 +0200

    Gjort EbmsMessageDetailService.fetchEbmsMessageDetails() smartere ved å gjøre equals-spørring fremfor like-spørring hvis innsendt readableId har 25 tegn.

--- src/main/kotlin/no/nav/emottak/eventmanager/service/EbmsMessageDetailService.kt
+++ src/main/kotlin/no/nav/emottak/eventmanager/service/EbmsMessageDetailService.kt
@@ -143,7 +143,10 @@ class EbmsMessageDetailService(
             Pair("Request ID", ebmsMessageDetailRepository.findByRequestId(Uuid.parse(id)))
         } else {
             log.info("Fetching message details by Readable ID: $id")
-            Pair("Readable ID", ebmsMessageDetailRepository.findByReadableIdPattern(id, 1000))
+            // Kjør spørring med = hvis id er 25 tegn, som er hele lengden på readableId:
+            if (id.length == 25) Pair("Readable ID", ebmsMessageDetailRepository.findByReadableId(id))
+            // Ellers kjører vi med like, som er MYE tregere:
+            else Pair("Readable ID", ebmsMessageDetailRepository.findByReadableIdPattern(id, 2))
         }
 
         if (messageDetails == null) {
